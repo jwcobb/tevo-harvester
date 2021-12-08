693ab8444cfda5ef5401c673272f6b1600726aad
J Cobb <777522+jwcobb@users.noreply.github.com>
2021-12-08 10:45:20 -0700

    Remove unneccessary laravel/sanctum

--- app/Models/User.php
+++ app/Models/User.php
@@ -6,14 +6,13 @@ use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
-use Laravel\Sanctum\HasApiTokens;
 
 /**
  * @mixin IdeHelperUser
  */
 class User extends Authenticatable
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasFactory, Notifiable;
 
     /**
      * The attributes that are mass assignable.
